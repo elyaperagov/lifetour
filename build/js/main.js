6a779142380fc750573fbcf540d512dc5a7b0f14
Елья <piragof@gmail.com>
2020-04-10 13:15:02 +0400

    добавляет IIFE

--- build/js/main.js
+++ build/js/main.js
@@ -1,105 +1,96 @@
 /*eslint-disable*/
 'use strict';
-var offersSwiper = new Swiper('.offers__swiper-container', {
-  freeMode: true,
-  loop: true,
-  navigation: {
-    nextEl: '.offers__button-next',
-    prevEl: '.offers__button-prev',
-  },
-  breakpoints: {
-    1024: {
-      slidesPerView: 3,
-      spaceBetween: 20,
-      width: 1196,
+(function () {
+  var offersSwiper = new Swiper('.offers__swiper-container', {
+    freeMode: true,
+    loop: true,
+    navigation: {
+      nextEl: '.offers__button-next',
+      prevEl: '.offers__button-prev',
     },
-    768: {
-      slidesPerView: 2,
-      spaceBetween: 20,
-      width: 785,
-    },
-    320: {
-      slidesPerView: 2,
-      spaceBetween: 38,
-      width: 600,
-    },
-  }
-});
+    breakpoints: {
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 20,
+        width: 1196,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+        width: 785,
+      },
+      320: {
+        slidesPerView: 2,
+        spaceBetween: 38,
+        width: 600,
+      },
+    }
+  });
 
-offersSwiper.slideNext(2, false);
-offersSwiper.slidePrev(2, false);
-
-var instructorsSwiper = new Swiper('.instructors', {
-  loop: true,
-  freeMode: true,
-  navigation: {
-    nextEl: '.instructors__button-next',
-    prevEl: '.instructors__button-prev',
-  },
-  breakpoints: {
-    1024: {
-      slidesPerView: 5,
-      spaceBetween: 8,
+  var instructorsSwiper = new Swiper('.instructors', {
+    loop: true,
+    freeMode: true,
+    navigation: {
+      nextEl: '.instructors__button-next',
+      prevEl: '.instructors__button-prev',
     },
-    768: {
-      spaceBetween: 8,
-      slidesPerView: 'auto',
-    },
-    320: {
-      slidesPerView: 'auto',
-      spaceBetween: 6,
-    },
-  }
-});
+    breakpoints: {
+      1024: {
+        slidesPerView: 5,
+        spaceBetween: 8,
+      },
+      768: {
+        spaceBetween: 8,
+        slidesPerView: 'auto',
+      },
+      320: {
+        slidesPerView: 'auto',
+        spaceBetween: 6,
+      },
+    }
+  });
 
-instructorsSwiper.slideNext(3, false);
-instructorsSwiper.slidePrev(3, false);
-
-var reviewsSwiper = new Swiper('.reviews__inner', {
-  slidesPerView: 3,
-  spaceBetween: 17,
-  loop: true,
-  freeMode: true,
-  navigation: {
-    nextEl: '.reviews__button-next',
-    prevEl: '.reviews__button-prev',
-  },
-  breakpoints: {
-    1024: {
-      width: 1224,
-      slidesPerView: 3,
-      spaceBetween: 17,
+  var reviewsSwiper = new Swiper('.reviews__inner', {
+    slidesPerView: 3,
+    spaceBetween: 17,
+    loop: true,
+    freeMode: true,
+    navigation: {
+      nextEl: '.reviews__button-next',
+      prevEl: '.reviews__button-prev',
     },
-    768: {
-      slidesPerView: 2,
-      spaceBetween: 17,
-      width: 1000,
+    breakpoints: {
+      1024: {
+        width: 1224,
+        slidesPerView: 3,
+        spaceBetween: 17,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 17,
+        width: 1000,
+      },
+      320: {
+        width: 610,
+        slidesPerView: 2,
+      },
+    }
+  });
+
+  var gallerySwiper = new Swiper('.gallery', {
+    slidesPerView: 'auto',
+    spaceBetween: 5,
+    loop: true,
+    freeMode: true,
+    navigation: {
+      nextEl: '.gallery__button-next',
+      prevEl: '.gallery__button-prev',
     },
-    320: {
-      width: 610,
-      slidesPerView: 2,
-    },
-  }
-});
-
-reviewsSwiper.slideNext(7, true);
-reviewsSwiper.slidePrev(7, true);
-
-var gallerySwiper = new Swiper('.gallery', {
-  slidesPerView: 'auto',
-  spaceBetween: 5,
-  loop: true,
-  freeMode: true,
-  navigation: {
-    nextEl: '.gallery__button-next',
-    prevEl: '.gallery__button-prev',
-  },
-});
-
-gallerySwiper.slideNext(10, true);
-gallerySwiper.slidePrev(10, true);
+  });
 
 
-$(function(){
-  $("#feedback__phone-input").mask("+7(999) 999-9999");
-});
+  $(function () {
+    $('#feedback__phone-input').mask('+7(999) 999-9999');
+  });
+
+})();
