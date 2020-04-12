12a0a217ef5207a4fa52af207c816012526f7525
Елья <piragof@gmail.com>
2020-04-13 01:39:12 +0400

    все правки по замечаниям наставника

--- source/js/main.js
+++ source/js/main.js
@@ -1,100 +1,104 @@
 'use strict';
 (function () {
-  var offersSwiper = new Swiper('.offers__swiper-container', {
-    freeMode: true,
-    loop: true,
-    navigation: {
-      nextEl: '.offers__button-next',
-      prevEl: '.offers__button-prev',
-    },
-    breakpoints: {
-      1024: {
-        slidesPerView: 3,
-        spaceBetween: 20,
-        width: 1196,
+  if (document.querySelector('.offers__swiper-container')) {
+    var offersSwiper = new Swiper('.offers__swiper-container', {
+      freeMode: true,
+      loop: true,
+      navigation: {
+        nextEl: '.offers__button-next',
+        prevEl: '.offers__button-prev',
       },
-      768: {
-        slidesPerView: 2,
-        spaceBetween: 20,
-        width: 785,
-      },
-      320: {
-        slidesPerView: 2,
-        spaceBetween: 38,
-        width: 600,
-      },
-    }
-  });
+      breakpoints: {
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 20,
+          width: 1196,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+          width: 785,
+        },
+        320: {
+          slidesPerView: 2,
+          spaceBetween: 38,
+          width: 600,
+        },
+      }
+    });
+    offersSwiper.mousewheel.enable();
+  }
 
-  offersSwiper.mousewheel.enable();
-
-  var instructorsSwiper = new Swiper('.instructors', {
-    loop: true,
-    freeMode: true,
-    navigation: {
-      nextEl: '.instructors__button-next',
-      prevEl: '.instructors__button-prev',
-    },
-    breakpoints: {
-      1024: {
-        slidesPerView: 5,
-        spaceBetween: 8,
+  if (document.querySelector('.instructors')) {
+    var instructorsSwiper = new Swiper('.instructors', {
+      loop: true,
+      freeMode: true,
+      navigation: {
+        nextEl: '.instructors__button-next',
+        prevEl: '.instructors__button-prev',
       },
-      768: {
-        spaceBetween: 8,
-        slidesPerView: 'auto',
+      breakpoints: {
+        1024: {
+          slidesPerView: 5,
+          spaceBetween: 8,
+        },
+        768: {
+          spaceBetween: 8,
+          slidesPerView: 'auto',
+        },
+        320: {
+          slidesPerView: 'auto',
+          spaceBetween: 6,
+        },
+      }
+    });
+    instructorsSwiper.lazy.load();
+  }
+
+
+  if (document.querySelector('.reviews__inner')) {
+    var reviewsSwiper = new Swiper('.reviews__inner', {
+      slidesPerView: 3,
+      spaceBetween: 17,
+      loop: true,
+      freeMode: true,
+      navigation: {
+        nextEl: '.reviews__button-next',
+        prevEl: '.reviews__button-prev',
       },
-      320: {
-        slidesPerView: 'auto',
-        spaceBetween: 6,
+      breakpoints: {
+        1024: {
+          width: 1224,
+          slidesPerView: 3,
+          spaceBetween: 17,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 17,
+          width: 1000,
+        },
+        320: {
+          width: 610,
+          slidesPerView: 2,
+        },
+      }
+    });
+    reviewsSwiper.keyboard.enable();
+  }
+
+  if (document.querySelector('.reviews__inner')) {
+    var gallerySwiper = new Swiper('.gallery', {
+      slidesPerView: 'auto',
+      spaceBetween: 5,
+      loop: true,
+      freeMode: true,
+      navigation: {
+        nextEl: '.gallery__button-next',
+        prevEl: '.gallery__button-prev',
       },
-    }
-  });
-
-  instructorsSwiper.lazy.load();
-
-  var reviewsSwiper = new Swiper('.reviews__inner', {
-    slidesPerView: 3,
-    spaceBetween: 17,
-    loop: true,
-    freeMode: true,
-    navigation: {
-      nextEl: '.reviews__button-next',
-      prevEl: '.reviews__button-prev',
-    },
-    breakpoints: {
-      1024: {
-        width: 1224,
-        slidesPerView: 3,
-        spaceBetween: 17,
-      },
-      768: {
-        slidesPerView: 2,
-        spaceBetween: 17,
-        width: 1000,
-      },
-      320: {
-        width: 610,
-        slidesPerView: 2,
-      },
-    }
-  });
-
-  reviewsSwiper.keyboard.enable();
-
-  var gallerySwiper = new Swiper('.gallery', {
-    slidesPerView: 'auto',
-    spaceBetween: 5,
-    loop: true,
-    freeMode: true,
-    navigation: {
-      nextEl: '.gallery__button-next',
-      prevEl: '.gallery__button-prev',
-    },
-  });
-
-  gallerySwiper.keyboard.enable();
-
+    });
+    gallerySwiper.keyboard.enable();
+  }
 
   $(function () {
     $('#feedback__phone-input').mask('+7(999) 999-9999');
